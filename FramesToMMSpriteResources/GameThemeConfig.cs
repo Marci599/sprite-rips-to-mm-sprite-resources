47ec8dfcd9db2a93796896fad9b76af7848cf3fc
Marci599 <katona.m.b@gmail.com>
2026-03-13 13:33:47 +0100

    renaming project folder

---
+++ FramesToMMSpriteResources/GameThemeConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace FramesToMMSpriteResources
+{
+    public class GameThemeConfig : ParentConfig
+    {
+        [JsonPropertyName("is_hd")]
+        public bool IsHd = true;
+
+        [JsonIgnore]
+        public Dictionary<string, SubjectConfig>? SubjectConfigs = [];
+
+        public GameThemeConfig() { }
+
+        public GameThemeConfig(bool isHd, bool isExpanded)
+        {
+            this.IsHd = isHd;
+            this.IsExpanded = isExpanded;
+        }
+    }
+}
---
+++ FramesToMMSpriteResources/ParentConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace FramesToMMSpriteResources
+{
+    public abstract class ParentConfig
+    {
+        [JsonPropertyName("is_expanded")]
+        public bool IsExpanded = false;
+    }
+}
